b019a50b04e834b825488bea3971cfb45200ee60
Thuan Tran <thuantran190200@gmail.com>
2022-05-18 15:55:44 +0700

    up

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+alert("Wellcome to Web");
+
+var sizemap = document.getElementsByClassName('map');
+
+var heading1 = document.querySelector('h1');
+heading1.className = 'heading-1'
+
+var heading2 = document.querySelector('h2');
+
+heading2.className = 'heading-2';
+
+console.log(heading2);
+console.log(sizemap);  
